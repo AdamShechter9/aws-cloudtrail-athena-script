5a6e1302efbe34408a3067a358a9357d1e4c652e
Adam Shechter <adam.s.develop@gmail.com>
2019-02-18 12:19:16 -0800

    Added MIT license.

--- cloudtrail_build.py
+++ cloudtrail_build.py
@@ -1,17 +1,21 @@
 #!/bin/python3
 """
-At1Security - Adam Shechter
+At1
+Adam Shechter
 
-Script to generate resources and start CloudTrail logging on AWS
+Python script to generate all resources needed to start CloudTrail logging on AWS
 
-Resources created:
-cloutrail log
-s3 bucket
-cloudwatch log group
-iam role
-athena database (if not existing)
-athena table in database
+
+
+# Copyright <2019> <At1 LLC>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software),
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 """
+
 
 import json
 import boto3
@@ -269,7 +273,7 @@ def randomstring():
 if __name__ == '__main__':
     args = sys.argv[1:]
     if not args:
-        print("This program generates regional CloudTrail for an AWS account\nusage: [profile_name] [account] [region_name]")
+        print("This script auto-generates regional CloudTrail logs and makes them available in Athena for querying\nusage: [profile_name] [account] [region_name]")
         sys.exit(1)
     else:
         profile_name = args[0]
